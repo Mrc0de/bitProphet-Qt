efcb719490c1866d595cb068fb962cedf6185618
MrC0de <MrC0de@geekprojex.com>
2018-03-27 12:49:12 -0600

    chart fixes

--- bppricegraph.cpp
+++ bppricegraph.cpp
@@ -33,15 +33,24 @@ void bpPriceGraph::say(QString sayThis) {
 
 void bpPriceGraph::loadPrices(QList<QString> newSeries,QList<QString> timeSeries) {
     say("Loading...");
-    for(int z=0;z<newSeries.count();z++){
-        mLineSeriesList->at(mLineSeriesList->count()-1)->append(mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(z)).toDouble(),newSeries.at(z).toDouble());
-        //say("Plotting y:" + newSeries.at(z) + " x:" + mParent->mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(z)));
+
+    for(int z=0;z<newSeries.count();z++){        
+        //std::cout<<std::endl<<timeSeries.at(z).toStdString()<<std::endl;
+        QDateTime dt = QDateTime::fromString(timeSeries.at(z),"yyyy-MM-dd hh:mm:ss");
+        mLineSeriesList->at(mLineSeriesList->count()-1)->append(QDateTime().fromString(timeSeries.at(z),"yyyy-MM-dd hh:mm:ss").toMSecsSinceEpoch(),newSeries.at(z).toDouble());
+        //say("Plotting y:" + newSeries.at(z) + " x:" + dt.toString() + " Valid: " + QString().setNum(dt.isValid()) );
     }
     mChart = new QChart();
     mChart->legend()->hide();
     mChart->addSeries(mLineSeriesList->at(mLineSeriesList->count()-1));
     mChart->createDefaultAxes();
+    mChart->removeAxis(mChart->axisX());
     mChart->setTitle(mName);
+    QDateTimeAxis *axisX = new QDateTimeAxis;
+    axisX->setTickCount(10);
+    axisX->setFormat("hh:mm:ss");
+    mChart->addAxis(axisX, Qt::AlignBottom);
+    mLineSeriesList->at(mLineSeriesList->count()-1)->attachAxis(axisX);
     //mChart->axisX()->setRange(0000,2459.59);
 
     mChartView = new QChartView(mChart,mWidgetParent);
@@ -57,12 +66,13 @@ void bpPriceGraph::loadPrices(QList<QString> newSeries,QList<QString> timeSeries
 void bpPriceGraph::reloadPrices(QList<QString> newSeries,QList<QString> timeSeries,int seriesIndex) {
     mLineSeriesList->at(seriesIndex)->clear();
     for(int z=0;z<newSeries.count();z++){
-        mLineSeriesList->at(seriesIndex)->append(mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(z)).toDouble(),newSeries.at(z).toDouble());
+        QDateTime dt = QDateTime::fromString(timeSeries.at(z),"yyyy-MM-dd hh:mm:ss");
+        mLineSeriesList->at(seriesIndex)->append(QDateTime().fromString(timeSeries.at(z),"yyyy-MM-dd hh:mm:ss").toMSecsSinceEpoch(),newSeries.at(z).toDouble());
         //say("Plotting y:" + newSeries.at(z) + " x:" + mParent->mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(z)));
     }
-    mChart->axisX()->setRange(mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(0)).toDouble(),mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(newSeries.count()-1)).toDouble());
+    //mChart->axisX()->setRange(mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(0)).toDouble(),mTimeTool->getHourFromSqlTimeStamp(timeSeries.at(newSeries.count()-1)).toDouble());
     mChart->axisY()->setRange(lowestPrice(newSeries).toDouble()-0.05,highestPrice(newSeries).toDouble()+0.05);
-    mChart->createDefaultAxes();
+    //mChart->createDefaultAxes();
     mChartView->repaint();
 }
 
